2f9bc3972718c347afdbaa2faaedf5931193176e
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-18 14:00:15 +0100

    Test loadRobotsAction with robotsReducer function

---
+++ src/store/features/robots/robotsSlice.test.ts
@@ -0,0 +1,37 @@
+import { loadRobotsActionCreator, robotsReducer } from "./robotsSlice";
+
+const robots = [
+  {
+    name: "terminator",
+    image: "",
+    stats: {
+      speed: 5,
+      endurance: 6,
+    },
+    creationDate: new Date("2022-03-25"),
+  },
+];
+
+describe("Given a robotsReducer function", () => {
+  describe("When it receives a list with a robot with name 'terminator' and a loadRobotsAction", () => {
+    test("Then it should return that same list of robots, including the robot with name 'terminator'", () => {
+      const expectedRobots = [
+        {
+          name: "terminator",
+          image: "",
+          stats: {
+            speed: 5,
+            endurance: 6,
+          },
+          creationDate: new Date("2022-03-25"),
+        },
+      ];
+
+      const loadRobotsAction = loadRobotsActionCreator(robots);
+
+      const newRobots = robotsReducer(robots, loadRobotsAction);
+
+      expect(newRobots).toStrictEqual(expectedRobots);
+    });
+  });
+});
