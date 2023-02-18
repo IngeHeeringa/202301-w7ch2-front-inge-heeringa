506a04b6af90ab17c51c228e2055fb05f737161f
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-18 17:15:46 +0100

    Add GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
+import GlobalStyles from "./GlobalStyles";
 import { store } from "./store";
 
 const container = document.getElementById("root")!;
@@ -10,6 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
