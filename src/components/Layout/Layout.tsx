89cc88dc43469db7034326e422ce86e196dce1e2
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-19 13:37:59 +0100

    Create Layout component structure

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import Robots from "../Robots/Robots";
+
+const Layout = () => {
+  return (
+    <>
+      <header className="main-header">
+        <h1 className="main-title">Robots</h1>
+      </header>
+      <Robots />
+    </>
+  );
+};
+
+export default Layout;
