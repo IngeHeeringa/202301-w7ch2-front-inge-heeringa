a4119c45b9b264820326802a8fc37f8cd3fc1e3c
Alexander <aguillenhernandez95@gmail.com>
2023-02-19 21:50:33 +0100

    Add DeleteAction component and its test

---
+++ src/components/DeleteAction/DeleteActionStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const DeleteActionStyled = styled.button`
+  position: absolute;
+  color: var(--accentColor);
+  font-size: 45px;
+  right: 5%;
+  top: 2%;
+`;
+
+export default DeleteActionStyled;
