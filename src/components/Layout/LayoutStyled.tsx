920db6716f6dcfdf8a5f4ea7b5bb5328ec5b3acb
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-19 13:38:42 +0100

    Add styles to Layout component

---
+++ src/components/Layout/LayoutStyled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  min-height: 100vh;
+  padding: 0 50px 50px 50px;
+
+  .main-title {
+    font-family: var(--displayFont);
+    color: var(--accentColor);
+    font-size: 4rem;
+    font-weight: normal;
+  }
+`;
+
+export default LayoutStyled;
