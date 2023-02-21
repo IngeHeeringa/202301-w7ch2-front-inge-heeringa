5e2c5ea151cf12e92a2d1802ee04c58f34b575c6
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-21 20:53:11 +0100

    Create React router and change Layout and index to match routes

---
+++ src/router/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import HomePage from "../pages/HomePage";
+
+export const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "/", element: <HomePage /> }],
+  },
+];
+
+export const router = createBrowserRouter(routes);
